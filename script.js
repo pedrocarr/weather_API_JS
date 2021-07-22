330196dff2383441241b8cc46ad963bc77e50c92
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-07-22 17:44:40 -0300

    add json object to the screen

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-document.querySelector('.busca').addEventListener('submit', (event)=>{
+document.querySelector('.busca').addEventListener('submit', async (event) => {
   event.preventDefault();
 
-  let input = document.querySelector('#searchInput').ariaValueMax;
+  let input = document.querySelector('#searchInput').value;
 
-  if(input !== '') {
-    showWarning('Loading...')
+  if (input !== '') {
+    showWarning('Loading...');
   }
 
-  let url = `api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=df2424834f1e3d997a4510df1958d30b`
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=df2424834f1e3d997a4510df1958d30b&units=metric&lang=en`
+
+  let results = await fetch(url);
+  let json = await results.json();
+
+  if (json.cod === 200) {
+    showInfo({
+      name : json.name,
+      country: json.sys.country,
+      temp: json.main.temp,
+      tempIcon: json.weather[0].icon,
+      windSpeed: json.wind.speed,
+      windAngle: json.wind.deg
+    });
+  } else {
+    showWarning('We did not find this location.');
+  }
 
 
-} );
+});
 
+function showInfo(json) {
+  showWarning('');
+
+  document.querySelector('.resultado').style.display = 'block';
+  document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country};`
+}
 
 function showWarning(msg) {
   document.querySelector('.aviso').innerHTML = msg;
