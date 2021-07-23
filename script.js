3f0284c34b7eb8ec573d202ff4d455c0b2b41855
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-07-23 10:46:19 -0300

    project finished

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 document.querySelector('.busca').addEventListener('submit', async (event) => {
   event.preventDefault();
 
@@ -7,7 +9,7 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
     showWarning('Loading...');
   }
 
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=${API_KEY}&units=metric&lang=en`
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=df2424834f1e3d997a4510df1958d30b&units=metric&lang=en`
 
   let results = await fetch(url);
   let json = await results.json();
@@ -22,6 +24,7 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
       windAngle: json.wind.deg
     });
   } else {
+    clearInfo();
     showWarning('We did not find this location.');
   }
 
@@ -30,16 +33,23 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
 
 function showInfo(json) {
   showWarning('');
-
-  document.querySelector('.resultado').style.display = 'block';
+  
   document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
   document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`;
   document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km</span>`;
-
-
-  document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+  
+  
+  document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
+  document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`;
+  document.querySelector('.resultado').style.display = 'block';
 }
 
 function showWarning(msg) {
   document.querySelector('.aviso').innerHTML = msg;
+}
+
+function clearInfo() {
+  showWarning('');
+  document.querySelector('.resultado').style.display = 'none';
+
 }
